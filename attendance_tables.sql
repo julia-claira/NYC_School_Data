2efd5162afb0443ab2b4feb1ccfb4e18910e3b9f
Claira J <julia.claira@gmail.com>
2021-06-12 11:32:04 -0700

    finsihed tabl

--- attendance_tables.sql
+++ attendance_tables.sql
@@ -1,25 +1,12 @@
 create table school_attendance (
 	school_id VARCHAR PRIMARY KEY,
-	enrolled_2015_2016 INT,
-	present_2015_2016 INT,
-	absent_2015_2016 INT,
-	enrolled_2016_2017 INT,
-	present_2016_2017 INT,
-	absent_2016_2017 INT,
-	enrolled_2017_2018 INT,
-	present_2017_2018 INT,
-	absent_2017_2018 INT,
-	enrolled_overall INT,
-	present_overall INT,
-	absent_overall INT);
+	enrolled_2015_2018 INT,
+	present_2015_2018 INT,
+	absent_2015_2018 INT,
+	attenance_prc float);
+
 	
-create table school_attendance_2015_16 (
-	school_id VARCHAR PRIMARY KEY,
-	enrolled_2015_2016 INT,
-	present_2015_2016 INT,
-	absent_2015_2016 INT);
-	
-select * from school_attendance_2015_16
+select * from school_attendance
 
 
 
