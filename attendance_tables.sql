86b3dac72b81b24fe977435f7fb47f4cd5107e91
Claira J <julia.claira@gmail.com>
2021-06-15 15:00:35 -0700

    combined Galen's dataframe into joint

--- attendance_tables.sql
+++ attendance_tables.sql
@@ -28,6 +28,40 @@ Create Table safety(
 	total_crime float
 	);
 	
+create table cleaned_df
+	("school id" varchar(10),
+	 "School Name" varchar,
+	 "Borough" varchar(20),
+	 "Building Code" varchar,
+	 "Street Address" varchar,
+	 "City" varchar,
+	 "State" char(2),
+	 "Zip Code" integer,
+	 "Latitude" decimal,
+	 "Longitude" decimal,
+	 "Start Time" varchar,
+	 "End Time" varchar,
+	 "Enrollment" integer,
+	 "White" Varchar,
+	 "Black" varchar,
+	 "Hispanic" varchar,
+	 "Asian" varchar,
+	 "Avg Score Math" integer,
+	 "Avg Score Reading" integer,
+	 "Avg Score Writing" integer,
+	 "Tested Percent" varchar
+	);
+	
+Create Table school_scores(
+	"school id" varchar Primary Key,
+	"Mean Scale Score" float,
+	"School Name" varchar,
+	"Year" int
+	);
+	
+	drop table school_scores
+
+select * from school_scores
 
 
 
--- proj_2_table_joins.sql
+++ proj_2_table_joins.sql
@@ -3,6 +3,7 @@ select a."school id",a."School Name",a."Borough",a."Building Code",
 a."Street Address",a."City",a."State",a."Zip Code",a."Latitude",
 a."Longitude",a."Start Time", a."End Time", a."White",a."Black", a."Hispanic", a."Asian",
 a."Avg Score Math",a."Avg Score Reading", a."Avg Score Writing",a."Tested Percent",
+c."Mean Scale Score",
 d.enrolled_2015_2018,d.present_2015_2018,
 d.absent_2015_2018,d.attendance_prc,
 b."Major N",b."Orth N", b."NoCrim N",b."Prop N",
@@ -12,7 +13,7 @@ inner join safety as b on
 a."school id"=b."school id"
 inner join school_attendance as d on
 (b."school id" = d."school id")
+inner join school_scores as c on
+(d."school id" = c."school id")
 
 select * from joined_nyc_table
-
-show data_directory;
